4e72858b2660c813a152dc51fb98e9154740b381
Marcus Will <marcusxwill@gmail.com>
2024-04-30 16:14:49 +0200

    refactor led functions to class

--- tv-wall-rc-lights.ino
+++ tv-wall-rc-lights.ino
@@ -1,218 +1,144 @@
+#include <Arduino.h>
 #include <RCSwitch.h>
 
-RCSwitch mySwitch = RCSwitch();
+// Constants
+#define DEFAULT_FADE_INTERVAL 20
 
-int LED_1 = 9;
-int LED_2 = 10;
-int POTI_PIN = 2;
+// Ease functions
+float easeOutBack(float x) {
+    float c1 = 1.70158;
+    float c3 = c1 + 1;
+    return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
+}
+
+float easeOutQuad(float x) {
+    return 1 - (1 - x) * (1 - x);
+}
+
+float easeInOutQuad(float x) {
+    return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
+}
+
+// LED class
+class LED {
+public:
+    LED(int pin, float amplitude = 1.0) 
+        : pin(pin), amplitude(amplitude), brightness(0),
+          fadeComplete(true), t(0.0) {
+        pinMode(pin, OUTPUT);
+    }
+
+    void setBrightness(int value) {
+        brightness = constrain(value, 0, 255);
+        analogWrite(pin, brightness * amplitude);
+    }
+
+    int getBrightness() const {
+        return brightness;
+    }
+
+    void fadeUp(int duration, int interval = DEFAULT_FADE_INTERVAL) {
+        if (fadeComplete) {
+            fadeComplete = false;
+            t = 0.0;
+        }
+
+        if (millis() - lastFadeTime >= interval && t <= 1.0) {
+            lastFadeTime = millis();
+            int newBrightness = int(255 * easeOutBack(t));
+            setBrightness(newBrightness);
+            t += float(interval) / duration;
+
+            if (t > 1.0) {
+                fadeComplete = true;
+            }
+        }
+    }
+
+    void fadeDown(int duration, int interval = DEFAULT_FADE_INTERVAL) {
+        if (fadeComplete) {
+            fadeComplete = false;
+            t = 1.0;
+        }
+
+        if (millis() - lastFadeTime >= interval && t >= 0.0) {
+            lastFadeTime = millis();
+            int newBrightness = int(255 * easeInOutQuad(t));
+            setBrightness(newBrightness);
+            t -= float(interval) / duration;
+
+            if (t < 0.0) {
+                fadeComplete = true;
+            }
+        }
+    }
+
+    bool isFadeComplete() const {
+        return fadeComplete;
+    }
+
+private:
+    int pin;
+    float amplitude;
+    int brightness;
+    bool fadeComplete;
+    float t;
+    unsigned long lastFadeTime = 0;
+};
+
+// Main program
+RCSwitch mySwitch;
+LED led1(9);
+LED led2(10, 0.9);
 
 bool powerOn = false;
 int mode = 0;
 
-int potiValue = 0;
-int oldPotiValue = 0;
-int minPotiValue = 5;
-int maxPotiValue = 950;
-
-int brightness1 = 0;
-int brightness2 = 0;
-int maxBrightness = 100;
-
 unsigned long now;
-long prevDelay;
-long prevFadeLed1 = 0;
-long prevFadeLed2 = 0;
-float t1Up = 0.0;
-float t2Up = 0.0;
-float t1Down = 1.0;
-bool fadeComplete1 = true;
-bool fadeComplete2 = true;
-int fadeInterval = 20;
-
+unsigned long prevDelay;
+unsigned long prevFadeLed1;
+unsigned long prevFadeLed2;
 
 void setup() {
-  // Serial.begin(9600);
-  mySwitch.enableReceive(1);  // Receiver on interrupt 0 => that is pin #2
-  pinMode(LED_1, OUTPUT);
-  pinMode(LED_2, OUTPUT);
+    mySwitch = RCSwitch();
+    mySwitch.enableReceive(1);  // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop() {
-  now = millis();
-  pingRcSwitch();
-  if (powerOn) pingPoti();
-
-  switch (mode) {
-    case 0:
-      if (powerOn) {
-        analogWrite(LED_1, maxBrightness);
-        analogWrite(LED_2, maxBrightness / 2);
-      }
-      break;
-    case 1:
-      fadeUpAnimation();
-      break;
-    case 2:
-      fadeDown();
-      break;
-
-    default:
-      break;
-  }
-}
-
-void startDelayTimer() {
-  prevDelay = now;
-}
-
-void fadeUpAnimation() {
-  fadeUpOnce(1, 2000);
-  // offset delay
-  if (now - prevDelay >= 1000) {
-    fadeUpOnce(2, 6000);
-  }
-  // if (now - prevDelay >= 4000) {
-  //   blink();
-  // }
-  if (fadeComplete1 && fadeComplete2) {
-    mode = 0;
-  }
-}
-
-void fadeUpOnce(int led, int duration) {
-  if (!fadeComplete1 && led == 1) {
-    if (now - prevFadeLed1 >= fadeInterval) {
-      prevFadeLed1 = now;
-
-      // Serial.print("brightness1: ");
-      // Serial.println(brightness1);
-
-      if (t1Up <= 1.0) {
-        brightness1 = (int)(maxBrightness * easeOutBack(t1Up));
-        analogWrite(LED_1, brightness1);
-        t1Up += (float)fadeInterval / duration;
-      } else {
-        t1Up = 0.0;
-        fadeComplete1 = true;
-      }
-    }
-  }
-  if (!fadeComplete2 && led == 2) {
-    if (now - prevFadeLed2 >= fadeInterval) {
-      prevFadeLed2 = now;
-
-      // Serial.print("Fadeup 2: ");
-      // Serial.print(t2Up);
-      // Serial.print(", ");
-      // Serial.print("brightness2: ");
-      // Serial.println(brightness2);
-
-      if (t2Up <= 1.0) {
-        brightness2 = (int)(maxBrightness * easeOutQuad(t2Up));
-        analogWrite(LED_2, brightness2 / 2);
-        t2Up += (float)fadeInterval / duration;
-      } else {
-        t2Up = 0.0;
-        fadeComplete2 = true;
-      }
-    }
-  }
-}
-
-void fadeDown() {
-  if (!fadeComplete1 && now - prevFadeLed1 >= fadeInterval) {
-    prevFadeLed1 = now;
-    const float duration = 1500.0;
-
-    if (t1Down >= 0.0) {
-      brightness1 = (int)(maxBrightness * easeInOutQuad(t1Down));
-      brightness2 = (int)(maxBrightness * easeOutQuad(t1Down));
-      analogWrite(LED_1, brightness1);
-      analogWrite(LED_2, brightness2 / 2);
-      t1Down -= fadeInterval / duration;
-
-      // Serial.print("Fade down: ");
-      // Serial.println(brightness1);
+    now = millis();
+    if (mySwitch.available()) {
+        int receivedValue = mySwitch.getReceivedValue();
+        if (receivedValue == 8685765 || receivedValue == 8685767) {
+            powerOn = true;
+            mode = 1;
+            mySwitch.resetAvailable();
+        } else if (receivedValue == 8685773 || 8685775) {
+            powerOn = false;
+            mode = 2;
+            mySwitch.resetAvailable();
+        }
     }
 
-    if (t1Down < 0.0) {
-      t1Down = 1.0;
-      fadeComplete1 = true;
-      fadeComplete2 = true;
-      mode = 0;
+    if (powerOn) {
+        switch (mode) {
+            case 1:
+                led1.fadeUp(3000);
+                led2.fadeUp(6000);
+                if (led1.isFadeComplete() && led2.isFadeComplete()) {
+                    mode = 0;
+                }
+                break;
+
+            case 2:
+                led1.fadeDown(1500);
+                led2.fadeDown(1500);
+                if (led1.isFadeComplete() && led2.isFadeComplete()) {
+                    mode = 0;
+                }
+                break;
+
+            default:
+                break;
+        }
     }
-  }
-}
-
-void blink() {
-  analogWrite(LED_2, maxBrightness / 2);
-  delay(200);
-  analogWrite(LED_2, maxBrightness);
-}
-
-void pingRcSwitch() {
-  if (mySwitch.available()) {
-    if (mySwitch.getReceivedValue() == 8685765 || mySwitch.getReceivedValue() == 8685767) {
-      if (fadeComplete1 && fadeComplete2) {
-        startDelayTimer();
-        resetFade();
-        powerOn = true;
-        mode = 1;
-        // Serial.println("on");
-      }
-    }
-    if (mySwitch.getReceivedValue() == 8685773 || mySwitch.getReceivedValue() == 8685775) {
-      if (powerOn) {
-        startDelayTimer();
-        resetFade();
-        powerOn = false;
-        mode = 2;
-        // Serial.println("off");
-      }
-    }
-    mySwitch.resetAvailable();
-  }
-}
-
-void pingPoti() {
-  potiValue = analogRead(POTI_PIN);
-
-  //adjust only if poti value changes
-  if (oldPotiValue > potiValue + 4 || oldPotiValue < potiValue - 4) {
-
-    // adjust max min value of poti
-    if (potiValue < minPotiValue) {
-      minPotiValue = potiValue;
-    }
-    if (potiValue > maxPotiValue) {
-      maxPotiValue = potiValue;
-    }
-
-    maxBrightness = map(potiValue, minPotiValue, maxPotiValue, 18, 255);
-
-    oldPotiValue = potiValue;
-  }
-}
-
-void resetFade() {
-  fadeComplete1 = false;
-  fadeComplete2 = false;
-  t1Up = 0.0;
-  t2Up = 0.0;
-  t1Down = 1.0;
-}
-
-float easeOutBack(float x) {
-  float c1 = 1.70158;
-  float c3 = c1 + 1;
-
-  return 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2);
-}
-
-float easeOutQuad(float x) {
-  return 1 - (1 - x) * (1 - x);
-}
-
-float easeInOutQuad(float x) {
-  return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
 }
